d4f499fe659432e1b02ab6e93b98cc2eef030f38
veryfatwombat <veryfatwombat@gmail.com>
2020-10-21 06:47:56 +1100

    HTML5 + CSS3 + JavaScript

---
+++ main.js
@@ -0,0 +1,6 @@
+let header = document.querySelector('.header');
+let hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hamburgerMenu.addEventListener('click', function () {
+  header.classList.toggle('menu-open');
+})
